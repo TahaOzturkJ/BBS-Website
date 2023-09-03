b1c732197c01d60ade884df2b70ace7687143d3e
Taha Öztürk <taharey2001@gmail.com>
2023-09-03 22:49:30 +0300

    Main page updates

--- bbswebsite/script.js
+++ bbswebsite/script.js
@@ -1,50 +1,8 @@
-const previous = document.querySelector('.previous-slide-btn');
-const next = document.querySelector('.next-slide-btn');
-const images = document.querySelector('.slider').children;
-const totalImages = images.length-2;
-let currentIndex = 0;
+    $(document).ready(function () {
+        $('#myCarousel').carousel(); // Initialize the carousel
+    });
 
+    $(document).ready(function () {
+      $('#solutions-carousel').carousel(); // Initialize the carousel
+  });
 
-	// Event Listeners to previous and next buttons
-    previous.addEventListener('click', () => {
-      previousImage()
-    })
-
-    next.addEventListener('click', () => {
-      nextImage();
-    })
-    
-    
-    // Function to go to next Image
-    function nextImage(){
-
-      images[currentIndex].classList.remove('main');
-    	if(currentIndex == totalImages-1){
-        	currentIndex = 0;
-        }
-        else{
-        	currentIndex++;
-        }
-
-      images[currentIndex].classList.add('main');
-      
-    }
-
-    setInterval(()=>{
-    	nextImage();
-    },4000);
-    
-    // Function to go to previous Image
-    function previousImage(){
-
-      images[currentIndex].classList.remove('main');
-    	if(currentIndex == 0){
-        	currentIndex = totalImages-1;
-        }
-        else{
-        	currentIndex--;
-        }
-    
-      images[currentIndex].classList.add('main');
-
-    }
